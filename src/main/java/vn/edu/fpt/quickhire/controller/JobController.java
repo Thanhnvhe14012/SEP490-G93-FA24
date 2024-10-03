2131f673b9ce58efb55bfc986cf10ab6ec9bae44
Truongst0123 <truongnqhe140182@fpt.edu.vn>
2024-10-03 10:37:55 +0700

    add trang create job

--- src/main/java/vn/edu/fpt/quickhire/controller/JobController.java
+++ src/main/java/vn/edu/fpt/quickhire/controller/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JobController {
+    //JobController
     @GetMapping("/job/create")
     public String home(HttpSession session, Model model) {
 
