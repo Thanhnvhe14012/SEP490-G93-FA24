ccdbaccee55bb55b2c385a714c4a8c399a9b4c51
thanhtrung2505 <trungpthe140548@fpt.edu.vn>
2024-11-14 23:54:39 +0700

    fix cetificate

--- src/main/java/vn/edu/fpt/quickhire/entity/DTO/CetificateDTO.java
+++ src/main/java/vn/edu/fpt/quickhire/entity/DTO/CetificateDTO.java
@@ -10,4 +10,5 @@ public class CetificateDTO {
     private String organization;
     private String start;
     private String end;
+    private String link;
 }
--- src/main/java/vn/edu/fpt/quickhire/model/repository/CetificateRepository.java
+++ src/main/java/vn/edu/fpt/quickhire/model/repository/CetificateRepository.java
@@ -3,6 +3,9 @@ package vn.edu.fpt.quickhire.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.edu.fpt.quickhire.entity.Cetificate;
 
+import java.util.List;
+
 public interface CetificateRepository extends JpaRepository<Cetificate, Long> {
     Cetificate findCetificateById(Long id);
+    List<Cetificate> findAllByAccountId(Long id);
 }
