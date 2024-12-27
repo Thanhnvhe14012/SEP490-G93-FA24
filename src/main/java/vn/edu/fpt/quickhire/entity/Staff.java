a80c229fba7fedb68cb2fac0b6b9555d9b597b9e
thanhnvhe140127 <thanhnvhe140127@fpt.edu.vn>
2024-12-27 11:08:12 +0700

    update db

---
+++ src/main/java/vn/edu/fpt/quickhire/entity/Staff.java
@@ -0,0 +1,30 @@
+package vn.edu.fpt.quickhire.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "staff")
+@Getter
+@Setter
+public class Staff {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Mapping thông tin biến với tên cột trong Database
+    @Column(name = "industry_id")
+    private Long industryId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruiter_id", nullable = false)
+    private Recruiter recruiter;
+
+}
